c837be39049708b515901c86f7a8495325236106
Chauncey McAskill <chauncey@mcaskill.ca>
2021-11-03 17:08:15 -0400

    Add optional support for svgo
    Added:
    - NPM dependency svgo v2.8.0
    - Utility 'svgo.js' to use dynamic import to fetch SVGO, if available.
    
    Changed:
    - 'svgs.js' to conditionally process spritesheets with SVGO if it's available.

--- build/tasks/svgs.js
+++ build/tasks/svgs.js
@@ -2,6 +2,7 @@ import loconfig from '../../loconfig.json';
 import message from '../utils/message.js';
 import notification from '../utils/notification.js';
 import template from '../utils/template.js';
+import optimize from '../utils/svgo.js';
 import { basename } from 'node:path';
 import mixer from 'svg-mixer';
 
@@ -19,14 +20,26 @@ export const developmentMixerOptions = Object.assign({}, defaultMixerOptions);
 export const productionMixerOptions  = Object.assign({}, defaultMixerOptions);
 
 /**
- * @const {object} developmentSVGsArgs - The predefined `compileSVGs()` options for development.
- * @const {object} productionSVGsArgs  - The predefined `compileSVGs()` options for production.
+ * @const {object} defaultSVGOOptions     - The default shared SVGO options.
+ * @const {object} developmentSVGOOptions - The predefined SVGO options for development.
+ * @const {object} productionSVGOOptions  - The predefined SVGO options for production.
+ */
+export const defaultSVGOOptions = {
+};
+export const developmentSVGOOptions = Object.assign({}, defaultSVGOOptions);
+export const productionSVGOOptions  = Object.assign({}, defaultSVGOOptions);
+
+/**
+ * @const {object|boolean} developmentSVGsArgs - The predefined `compileSVGs()` options for development.
+ * @const {object|boolean} productionSVGsArgs  - The predefined `compileSVGs()` options for production.
  */
 export const developmentSVGsArgs = [
     developmentMixerOptions,
+    false,
 ];
 export const productionSVGsArgs  = [
     productionMixerOptions,
+    productionSVGOOptions,
 ];
 
 /**
@@ -35,9 +48,11 @@ export const productionSVGsArgs  = [
  * @async
  * @param  {object} [mixerOptions=null] - Customize the Mixer API options.
  *     If `null`, default production options are used.
+ * @param  {object|boolean} [svgoOptions=null] - Customize the SVGO processor API options.
+ *     If `null`, default production options are used.
  * @return {Promise}
  */
-export default async function compileSVGs(mixerOptions = null) {
+export default async function compileSVGs(mixerOptions = null, svgoOptions = null) {
     if (mixerOptions == null) {
         mixerOptions = productionMixerOptions;
     } else if (
@@ -45,6 +60,18 @@ export default async function compileSVGs(mixerOptions = null) {
         mixerOptions !== productionMixerOptions
     ) {
         mixerOptions = Object.assign({}, defaultMixerOptions, mixerOptions);
+    }
+
+    if (optimize) {
+        if (svgoOptions == null) {
+            svgoOptions = productionSVGOOptions;
+        } else if (
+            svgoOptions !== false &&
+            svgoOptions !== developmentSVGOOptions &&
+            svgoOptions !== productionSVGOOptions
+        ) {
+            svgoOptions = Object.assign({}, defaultSVGOOptions, svgoOptions);
+        }
     }
 
     loconfig.tasks.svgs.forEach(async ({
@@ -61,6 +88,10 @@ export default async function compileSVGs(mixerOptions = null) {
             outfile  = template(outfile);
 
             const result = await mixer(includes, mixerOptions);
+
+            if (optimize && svgoOptions) {
+                result.content = optimize(result.content, svgoOptions).data;
+            }
 
             await result.write(outfile);
 
